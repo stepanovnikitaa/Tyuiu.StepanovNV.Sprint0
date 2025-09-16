0e6921e021d458faea658c72a02539ee1f6d0dbd
Nikita Stepanov <Tyuiu.StepanovNV@outlook.com>
2025-09-16 22:10:04 +0300

    Task5

---
+++ Tyuiu.StepanovNV.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Tyuiu.StepanovNV.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtraction(int a, int b)
+        {
+            return (a - b);
+        }
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+                return -1;
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
