02964c839f0bf126e64e42b93e5da74bd254d53f
Nikita Stepanov <Tyuiu.StepanovNV@outlook.com>
2025-09-16 15:16:01 +0300

    Task1

---
+++ Tyuiu.StepanovNV.sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.StepanovNV.Sprint0.Task1.V0
+{
+    class Program   
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Игорь";
+            int age = 45;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
