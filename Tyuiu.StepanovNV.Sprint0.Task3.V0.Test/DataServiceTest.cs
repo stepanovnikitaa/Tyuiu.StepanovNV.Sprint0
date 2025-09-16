0fa835db67f9b241f072323592b194f9d678ee14
Nikita Stepanov <Tyuiu.StepanovNV@outlook.com>
2025-09-16 20:38:57 +0300

    Task3

---
+++ Tyuiu.StepanovNV.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tyuiu.StepanovNV.Sprint0.Task3.V0.Lib;
+
+
+namespace Tyuiu.StepanovNV.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
